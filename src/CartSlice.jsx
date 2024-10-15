2f4853e0d8c49058b9de9c9814256aac634d935f
Ashutosh gautam <ashutoshgautam3006@gmail.com>
2024-10-15 02:15:25 -0400

    final commit

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,28 +4,33 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    totalQuantity:0
   },
   reducers: {
     addItem: (state, action) => {
-    const { name, image, cost } = action.payload;
-    const existingItem = state.items.find(item => item.name === name);
-    if (existingItem) {
-        existingItem.quantity++;
-    } else {
-        state.items.push({ name, image, cost, quantity: 1 });
-    }
+    const {name, image, cost} = action.payload;
+    const existing=state.items.find((plant)=>plant.name===name);
+    if(existing){
+        existing.quantity++;
+      }else{
+        state.items.push({name,image,cost,quantity:1});
+      }
+      state.totalQuantity++;
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+        let item=state.items.find((plant)=>plant.name===action.payload);
+      state.items=state.items.filter((plant)=>plant.name!==action.payload); 
+      state.totalQuantity-=item.quantity;
     },
     updateQuantity: (state, action) => {
-        const { name, quantity } = action.payload;
-        const itemToUpdate = state.items.find(item => item.name === name);
-        if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
-        }
-    
-    },
+         const {name,quantity}=action.payload;
+      const existing=state.items.find((plant)=>plant.name===name);
+      if(existing){
+        let updateCost=quantity-existing.quantity;
+        existing.quantity=quantity;
+        state.totalQuantity+=updateCost;
+}
+ },
   },
 });
 
